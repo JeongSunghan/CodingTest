38edc10e7e4207855b0f951efcfc151c314ec612
sunghan <155530452+JeongSunghan@users.noreply.github.com>
2024-07-29 20:15:51 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/120898. 편지/편지.java
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -1,9 +1,6 @@
 class Solution {
-    public int solution(String message) {
-        int msg_lan = message.length();           
-        int msg_letter = msg_lan * 2;
-        
-        int answer = msg_letter;       
+    public int solution(String message) {        
+        int answer = message.length() * 2;
         return answer;
     }
 }
